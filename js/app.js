f1e753bf7f94b6c3973c4bb60ef96baaa7434f9d
Danielapariona <daniela.parionac@gmail.com>
2017-11-18 09:09:43 -0500

    method of counting letters and showing their status.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,24 @@
 var area = document.getElementById('insert-text');
 var button = document.getElementById('save');
 var list = document.getElementById('content-tweets');
+var count = document.getElementById('count');
+
+function stateLetter(countLetter) {
+  var minimumLetter = 10;
+  if(countLetter <= 10 && countLetter >= 0) {
+    count.className = "red";
+  } else if(countLetter < 0) {
+    count.className = "twitterBlue background-red";
+  } 
+}
+
+var countLetters = function() {
+  count.textContent = 140;
+  var maxLetter = 140;
+  var currentLetter = maxLetter - ((area.value).trim()).length;
+  count.textContent = currentLetter;
+  stateLetter(count.textContent);
+}
 
 var showTime = function () {
   var f = new Date();
@@ -30,4 +48,8 @@ function showTweet (event) {
   }
 }
 
-button.addEventListener('click', showTweet);
+
+window.onload = function() {
+  button.addEventListener('click', showTweet);
+  area.addEventListener('keydown', countLetters );
+}
